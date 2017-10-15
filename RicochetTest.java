6b5322fe36c9df23dac25a44a16d97b29cbde8c2
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-10-15 14:10:37 +0200

    Indentation

--- RicochetTest.java
+++ RicochetTest.java
@@ -1,8 +1,5 @@
 public class RicochetTest {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Version 1!!!");
-    }
-
+  public static void main(String[] args) {
+    System.out.println("Version 1!!!");
+  }
 }
