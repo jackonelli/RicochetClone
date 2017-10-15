1a42dc48d60b7bf29892cc2cde6494d9665872bb
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-10-15 20:14:53 +0200

    True and actual constants

--- src/Constants.java
+++ src/Constants.java
@@ -1,6 +1,6 @@
 public final class Constants{
     public final int SIZE = 16;
-    public final int NUMBER_OF_ROBOTS = 4;
+    public final int NUMBER_OF_ROBOTS = 5;
     public final int NORTH = 1;
     public final int EAST = 2;
     public final int SOUTH = 3;
