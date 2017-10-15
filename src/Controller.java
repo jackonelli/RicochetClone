c94bae626da252a2cf430afc57055df920740db7
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-10-15 19:10:35 +0200

    Removed shame features

--- src/Controller.java
+++ src/Controller.java
@@ -1,6 +1,7 @@
-public class Controller {
-    private static int final SIZE = 16;
-    private static int final NUMBER_OF_ROBOTS = 4;
+public class Controller
+{
+    private static final int SIZE = 16;
+    private static final int NUMBER_OF_ROBOTS = 4;
     private Model model(SIZE);
     private View view(SIZE);
     public Controller(Model model, View view){
@@ -11,8 +12,8 @@ public class Controller {
     int robotID;
     int direction;
     public void initView(model, view){
-        for(int row, row < SIZE, row++){
-            for(int col, col < SIZE, col++){
+        for(int row; row < SIZE; row++){
+            for(int col; col < SIZE; col++){
                 tile = model.getTile(row, col);
                 view.drawTile(row, col, tile);
             } // Cols
@@ -20,28 +21,30 @@ public class Controller {
     }
 
     public int[] move(int robotID, int direction){
-        int[NUMBER_OF_ROBOTS - 1][2] staticRobotPositions;
         int tilesMoved = 0;
-        int[2] currentPosition = model.whereIsRobot(robotID);
+        int[] currentPosition = new int[2];
+        int[] newPosition = new int[2];
+        currentPosition = model.whereIsRobot(robotID);
         boolean canMove = true;
-        boolean[4] tileHasWalls;
+        boolean[] tileHasWalls = new boolean[4];
         switch(direction){
             // North
-            case 1:
+            case 1 :
                 col = currentPosition[1];
                 row = currentPosition[0];
                 while(canMove){
                     tileHasWalls = model.getWalls(row + tilesMoved + 1, col);
                     wallBlocks = tileHasWalls[2];
                     // Access tile?
-                    tileHasRobot = model.hasRobot[row + tilesMoved + 1, col];
+                    tileHasRobot = model.hasRobot(row + tilesMoved + 1, col);
                     if(wallBlocks || tileHasRobot){
                         canMove = false;
                     } else {
                         tilesMoved++;
                     }
                 }
-                newPosition = {row + tilesMoved, col};
+                newPosition[0] = row + tilesMoved;
+                newPosition[1] = col;
             // East
             case 2:
                 col = currentPosition[1];
@@ -50,14 +53,15 @@ public class Controller {
                     tileHasWalls = model.getWalls(row, col + tilesMoved + 1);
                     wallBlocks = tileHasWalls[3];
                     // Access tile?
-                    tileHasRobot = model.hasRobot[row, col + tilesMoved + 1];
+                    tileHasRobot = model.hasRobot(row, col + tilesMoved + 1);
                     if(wallBlocks || tileHasRobot){
                         canMove = false;
                     } else {
                         tilesMoved++;
                     }
                 }
-                newPosition = {row, col + tilesMoved};
+                newPosition[0] = row;
+                newPosition[1] = col + tilesMoved;
             // South
             case 3:
                 col = currentPosition[1];
@@ -66,14 +70,15 @@ public class Controller {
                     tileHasWalls = model.getWalls(row + tilesMoved + 1, col);
                     wallBlocks = tileHasWalls[1];
                     // Access tile?
-                    tileHasRobot = model.hasRobot[row + tilesMoved + 1, col];
+                    tileHasRobot = model.hasRobot(row + tilesMoved + 1, col);
                     if(wallBlocks || tileHasRobot){
                         canMove = false;
                     } else {
                         tilesMoved++;
                     }
                 }
-                newPosition = {row + tilesMoved, col};
+                newPosition[0] = row - tilesMoved;
+                newPosition[1] = col;
             // West
             case 4:
                 col = currentPosition[1];
@@ -82,14 +87,15 @@ public class Controller {
                     tileHasWalls = model.getWalls(row, col + tilesMoved + 1);
                     wallBlocks = tileHasWalls[2];
                     // Access tile?
-                    tileHasRobot = model.hasRobot[row, col + tilesMoved + 1];
+                    tileHasRobot = model.hasRobot(row, col + tilesMoved + 1);
                     if(wallBlocks || tileHasRobot){
                         canMove = false;
                     } else {
                         tilesMoved++;
                     }
                 }
-                newPosition = {row, col + tilesMoved};
+                newPosition[0] = row;
+                newPosition[1] = col - tilesMoved;
         }
         return newPosition;
     }
