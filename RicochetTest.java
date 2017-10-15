66fd0da8ddcec2b037ce6b3fa66d6c4775497cf6
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-10-15 13:10:45 +0200

    Actual java code, wow.

---
+++ RicochetTest.java
@@ -0,0 +1,8 @@
+public class RicochetTest {
+
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Version 1!!!");
+    }
+
+}
