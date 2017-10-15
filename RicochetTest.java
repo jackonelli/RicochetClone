ef1783114732f5117f53b2156042639f324e70d9
Niklas Johansson <johansson.s.niklas@gmail.com>
2017-10-15 14:21:20 +0200

    Uppdaterar efter att vi fått det att funka på Daniels samt tar bort tidigare testfiler

--- RicochetTest.java
+++
@@ -1,8 +0,0 @@
-public class RicochetTest {
-
-    public static void main(String[] args) {
-        // Prints "Hello, World" to the terminal window.
-        System.out.println("Version 1!!!");
-    }
-
-}
