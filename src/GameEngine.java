337b0c058be24df4646b755e0caeb73c3abbf76b
Jonathan Fager <faaager@gmail.com>
2017-10-16 20:27:02 +0200

    Fixed error in GameEngine after cleanup

--- src/GameEngine.java
+++ src/GameEngine.java
@@ -2,8 +2,7 @@ public class GameEngine {
 
 	public static void main(String[] args) {
         // Prints "Hello, World" to the terminal window.
-		TestClass tc = new TestClass();
-        System.out.println(tc.getText());
+		System.out.println("Hello World!");
     }
 
 }
