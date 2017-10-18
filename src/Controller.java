04e4b8b1ab15a6c3fdd79dc26f26db094254c5f0
Jakob Lindqvist <jakob.lindqvist.92@gmail.com>
2017-10-18 08:58:05 +0200

    RunGame functionality

--- src/Controller.java
+++ src/Controller.java
@@ -1,35 +1,47 @@
-//import static Constants.*;
+import java.util.ArrayList;
+//import java.util.stream;
 
 public class Controller
 {
+
     public final int SIZE = 16;
     public final int NUMBER_OF_ROBOTS = 5;
+    public final int NUMBER_OF_GOALZ = 17;
     public final int NORTH = 1;
     public final int EAST = 2;
     public final int SOUTH = 3;
     public final int WEST = 4;
+    public final int BLUE = 1;
+    public final int RED = 2;
+    public final int GREEN = 3;
+    public final int YELLOW = 4;
+    public final int SILVER = 5;
+
     private Model model;
     private View view;
+
+    private ArrayList<Integer> unvisitedGoalz = new ArrayList<Integer>();
+    //private ArrayList<Integer> unvisitedGoalz = IntStream.range(0, NUMBER_OF_GOALZ - 1).toArray();
     public Controller(Model model, View view){
         this.model = model; // Array
         this.view = view;
     }
 
     public void runGame(Model model, View view){
-        view.redraw(model.getTiles());
+        //view.redraw(model);
+        //getNewGoal();
         //TODO: Get from user input
-        int robotID;
-        int direction;
-        move(robotID, direction);
-
+        int robotID = YELLOW;
+        int direction = NORTH;
+        //move(robotID, direction);
     }
 
-    private int[] move(int robotID, int direction){
+    private void move(int robotID, int direction){
         int tilesMoved = 0;
         boolean[] tileHasWalls = new boolean[4];
         boolean wallBlocks, tileHasRobot;
         int[] currentPosition = model.findRobot(robotID);
-        int[] newPosition;
+        int[] newPosition = new int[2];
         boolean canMove = true;
         int col, row;
         switch(direction){
@@ -102,10 +114,10 @@ public class Controller
                 newPosition[1] = col - tilesMoved;
 
         }
-        updateBoard(model, robotID, newPosition);
+        updateBoard(robotID, newPosition);
     }
 
-    private void updateBoard(Model model, int robotID, int[] newPosition){
-        model.setRobot(robotID, newPosition);
+    private void updateBoard(int robotID, int[] newPosition){
+        //model.setRobot(robotID, newPosition);
     }
 }
